92f8c35c2167518824d6fc127e742d8093199cde
swizzlestix09 <angely.rodriguez@gmail.com>
2021-10-02 17:23:07 -0400

    working on weird bug

--- dummydata.js
+++ dummydata.js
@@ -1,9 +1,9 @@
-var dummydata = [
-  "I’m not going to go to the gym today",
-  "I’m not going to do my hair today",
-  "Go to sephora and buy a new face",
-  "Return videos to blockbuster rental (28 years overdue)",
-  "I love my husband - but F*CK cooking tonight"
-]
+const dummydata = [
+  'I’m not going to go to the gym today',
+  'I’m not going to do my hair today',
+  'Go to sephora and buy a new face',
+  'Return videos to blockbuster rental (28 years overdue)',
+  'I love my husband - but F*CK cooking tonight',
+];
 
 export default dummydata;
--- src/ToDoList/TodoList.jsx
+++ src/ToDoList/TodoList.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import InputText from './InputText';
 
-const toDoList = ({ tasks }) => {
+const toDoList = (props) => {
   let listItems = [];
-  if (tasks.length === 0) {
-    listItems = <div>Im so empty. Like Kate Moss.</div>;
+  if (!props.tasks) {
+    listItems = <div>I'm so empty. Like Kate Moss.</div>;
+  } else {
+    listItems = props.todoList.map((task, i) => (
+      <li key={i.toString()} className="eachTask">
+        {task}
+        <input type="checkbox" />
+      </li>
+    ));
   }
-  listItems = tasks.todoList.map((task, i) => (
-    <li key={i.toString()} className="eachTask">
-      {task}
-      <input type="checkbox" />
-    </li>
-
-  ));
 
   return (
     <div>
