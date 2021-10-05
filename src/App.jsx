cba3074f7fa1120199e397b1f6165ffc7afa1bb3
swizzlestix09 <angely.rodriguez@gmail.com>
2021-10-04 20:53:28 -0400

    state blinking after re-render..

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import dummydata from '../dummydata';
-import TodoList from './ToDoList/TodoList';
-import InputText from './ToDoList/InputText';
+import React from "react";
+import dummydata from "../dummydata";
+import TodoList from "./ToDoList/TodoList";
+import InputText from "./ToDoList/InputText";
 
-class App extends React.Component {
+class App extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      currentToDo:  '',
+      currentToDo: "",
       todos: [...dummydata],
       completedTodos: [],
     };
@@ -15,17 +15,15 @@ class App extends React.Component {
     this.submitToDo = this.submitToDo.bind(this);
   }
 
-  shouldComponentUpdate(prevState, )
-
   textChange(e) {
     let current = e.target.value;
-    this.setState( { currentToDo: current} );
+    this.setState({ currentToDo: current });
     e.preventDefault;
   }
 
   submitToDo() {
     let current = this.state.currentToDo;
-    let currentToDos = [ current, ...this.state.todos];
+    let currentToDos = [current, ...this.state.todos];
     this.setState({ todos: [...currentToDos] });
 
     // this.setState( (current, currentToDos) => {
@@ -34,7 +32,7 @@ class App extends React.Component {
 
   render() {
     const { todos } = this.state;
-    console.log('todos rendered ', todos)
+    console.log("todos rendered ", todos);
     return (
       <div>
         <h1 className="todotitle">Definitely Not a Todo App</h1>
